7dcc958976f9b7be9590c9490fe05ab92dd23835
csrgxtu <1246506786@qq.com>
2015-04-20 09:53:18 +0800

    add api/controllers/UserController.js structure

---
+++ api/controllers/UserController.js
@@ -0,0 +1,30 @@
+/**
+ * Author: Archer Reilly
+ * Date: 20/Apr/2015
+ * File: UserController.js
+ * Desc: controllers for User management
+ *
+ * Produced By Ebang
+ */
+
+module.exports = {
+  index: function(req, res) {
+
+  },
+
+  create: function(req, res) {
+
+  },
+
+  read: function(req, res) {
+
+  },
+
+  update: function(req, res) {
+
+  },
+
+  delete: function(req, res) {
+
+  },
+};
